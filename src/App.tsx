ede5eaeba0c1596bfc74c13742081152cd2647f5
Paulos <paulosgirmachew28@gmail.com>
2026-08-10 16:11:57 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-blue-500 w-full min-h-screen grid place-content-center">
+      <div className="bg-red-500 w-32 h-32 rounded-full flex items-center justify-center">
+        <h1 className="text-white font-bold">Login</h1>
+      </div>
+      <h1 className="text-white text-xl mt-4 text-center">Welcome to My App</h1>
+      <h2 className="bg-amber-950 text-blue-400">
+        this is the official page for bebi
+      </h2>
+    </div>
+  );
+}
+export default App;
