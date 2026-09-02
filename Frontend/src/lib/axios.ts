fd085f3851873ab0e16251f64c016cf4267fea3c
Paulos <paulosgirmachew28@gmail.com>
2026-09-03 00:10:05 +0300

    Fix bugs for redeployment

--- Frontend/src/lib/axios.ts
+++ Frontend/src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8989/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8989/api",
   withCredentials: true,
 });
 export default api;
-//import.meta.env.VITE_API_URL ||
